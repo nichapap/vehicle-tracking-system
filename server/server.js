a1f149f5e405197e131ed6f22725d99e058116c1
Nichapa Pakhakorntanatorn <nichapa.pakh@gmail.com>
2022-01-22 02:38:19 +0700

    Revert "Restructure server and modify package.json"
    This reverts commit 8c7149230f07c150e114255f2f9f5795fcdf5e9b.

--- server/server.js
+++
@@ -1,7 +0,0 @@
-import database from './database.js';
-import makeApp from './app.js';
-
-const port = 9000;
-
-const app = makeApp(database);
-app.listen(port, () => console.log(`Server is running on port ${port}`));
